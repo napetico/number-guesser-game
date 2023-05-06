266364dcf5be2f73601ac02962854d418a7c3dbc
napetico <128691471+napetico@users.noreply.github.com>
2023-05-06 17:21:53 +0200

    Include humanGuess Checker

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Check human guess correct number
-const humanGuessCheck = () => {
-    if (humanGuess < 0 || humanGuess > 9) {
-        return "You guess is out of range. Please enter a number betweeen 0 and 9."
-    }
-}
-
 // Generate a random number
 const generateTarget = () => {
     let randomNumber = Math.floor((Math.random() * 10));
@@ -17,21 +10,20 @@ const generateTarget = () => {
 
 // Compare numbers and select winner
 const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
-    /* Check human guess correct number
-    const humanGuessCheck = () => {
-        if (humanGuess < 0 || humanGuess > 9) {
-            return "You guess is out of range. Please enter a number betweeen 0 and 9."
-        }
-    }*/
-    const humanDifference = Math.abs(humanGuess - targetGuess);
-    const computerDifference = Math.abs(computerGuess - targetGuess);
-    return humanDifference <= computerDifference;
-    /* This is a more concise way to say:
-        if (humanDifference <= computerDifference) {
-            return true;
-        } else {
-            return false;
-        }*/
+    // Check human guess correct number
+    if (humanGuess < 0 || humanGuess > 9) {
+        alert("Please enter a number between 0 and 9.");
+    } else {
+        const humanDifference = Math.abs(humanGuess - targetGuess);
+        const computerDifference = Math.abs(computerGuess - targetGuess);
+        return humanDifference <= computerDifference;
+        /* This is a more concise way to say:
+            if (humanDifference <= computerDifference) {
+                return true;
+            } else {
+                return false;
+            }*/
+    }
 }
 
 // Update winner's score
